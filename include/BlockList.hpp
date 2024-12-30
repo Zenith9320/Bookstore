f1d986b29b9e9db6d002ed20ab07ba3e79b9db65
Zenith9320 <zhangentong@sjtu.edu.cn>
2024-12-30 18:51:06 +0800

    use make_unique

--- include/BlockList.hpp
+++ include/BlockList.hpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <cstring>
 #include <set>
+#include <memory>
 
 using std::string;
 
@@ -15,6 +16,14 @@ template <class T>
 struct Key_Value {//存储键值对
     char key[MAX_STRING];
     T value;
+
+    Key_Value & operator = (const Key_Value &other) {
+        if (this != &other) {
+            strcpy(key, other.key);
+            value = other.value;
+        }
+        return *this;
+    }
 
     Key_Value(){
         memset(key, 0, sizeof(key));
@@ -148,8 +157,8 @@ public:
 
     void Insert(const std::string &key, const T &value) {
         Key_Value<T> KeyValue(key, value);
-        Index index;
-        Block<T> block;
+        auto index = std::make_unique<Index>();
+        auto block = std::make_unique<Block<T>>();
         //获取索引文件中的Index数量
         int block_pos = 0, index_number;
         index_file.get_info(index_number, 1);
@@ -157,145 +166,145 @@ public:
         int index_pos = 0;
         index_file.get_info(index_pos, 2);
         if (index_number == 0) {
-            block.KeyValues[0] = KeyValue;
-            block.KeyValue_num++;
-            index.KeyValue_num++;
-            strcpy(index.beg_key, key.c_str());
-            strcpy(index.end_key, key.c_str());
-            index.offset = block_file.write(block);
-            index_file.write_info(index_file.write(index), 2);
+            block->KeyValues[0] = KeyValue;
+            block->KeyValue_num++;
+            index->KeyValue_num++;
+            strcpy(index->beg_key, key.c_str());
+            strcpy(index->end_key, key.c_str());
+            index->offset = block_file.write(*block);
+            index_file.write_info(index_file.write(*index), 2);
             index_file.write_info(1, 1);
         }
         for (int i = 0; i < index_number; ++i) {
-            index_file.read(index, index_pos);
+            index_file.read(*index, index_pos);
             // 插入键值对
-            if (strcmp(index.end_key, key.c_str()) >= 0 || index.next_offset == -1) {
-                block_pos = index.offset;
-                block_file.read(block, block_pos);
+            if (strcmp(index->end_key, key.c_str()) >= 0 || index->next_offset == -1) {
+                block_pos = index->offset;
+                block_file.read(*block, block_pos);
                 // 若Block满了就需要裂块
-                if (block.KeyValue_num == BLOCK_SIZE) {
-                    Block<T> new_block;
-                    Index new_index;
-                    splitBlock(block, new_block);//裂块
+                if (block->KeyValue_num == BLOCK_SIZE) {
+                    auto new_block = std::make_unique<Block<T>>();
+                    auto new_index = std::make_unique<Index>();
+                    splitBlock(*block, *new_block);//裂块
                     // 插入键值对
-                    if (cmp(KeyValue, new_block.KeyValues[0])) {
-                        insertKeyvalue(new_block, KeyValue);
+                    if (cmp(KeyValue, new_block->KeyValues[0])) {
+                        insertKeyvalue(*new_block, KeyValue);
                     } else {
-                        insertKeyvalue(block, KeyValue);
+                        insertKeyvalue(*block, KeyValue);
                     }
 
                     // 更新原数据快
-                    block_file.update(block, block_pos);
+                    block_file.update(*block, block_pos);
                     // 更新数据块的键值对数量
-                    index.KeyValue_num = block.KeyValue_num;
-                    new_index.KeyValue_num = new_block.KeyValue_num;
+                    index->KeyValue_num = block->KeyValue_num;
+                    new_index->KeyValue_num = new_block->KeyValue_num;
                     // 更新开始结束键
-                    strcpy(new_index.beg_key, new_block.KeyValues[0].key);
-                    strcpy(new_index.end_key, new_block.KeyValues[new_block.KeyValue_num - 1].key);
-                    strcpy(index.beg_key, block.KeyValues[0].key);
-                    strcpy(index.end_key, block.KeyValues[block.KeyValue_num - 1].key);
+                    strcpy(new_index->beg_key, new_block->KeyValues[0].key);
+                    strcpy(new_index->end_key, new_block->KeyValues[new_block->KeyValue_num - 1].key);
+                    strcpy(index->beg_key, block->KeyValues[0].key);
+                    strcpy(index->end_key, block->KeyValues[block->KeyValue_num - 1].key);
                     
                     // 更新链表
-                    new_index.offset = block_file.write(new_block);
-                    new_index.next_offset = index.next_offset;
-                    index.next_offset = index_file.write(new_index);
+                    new_index->offset = block_file.write(*new_block);
+                    new_index->next_offset = index->next_offset;
+                    index->next_offset = index_file.write(*new_index);
 
                     // 写回更新后的原索引
-                    index_file.update(index, index_pos);
+                    index_file.update(*index, index_pos);
 
                     // 更新索引文件中存储的索引数量，增加一个新的索引
                     index_file.write_info(index_number + 1, 1);
                     break;
                 } else {
-                    insertKeyvalue(block, KeyValue);
-                    index.KeyValue_num = block.KeyValue_num;
-                    if (strcmp(block.KeyValues[block.KeyValue_num - 1].key, index.end_key) != 0 || block.KeyValue_num == 1) {
-                        strcpy(index.end_key, block.KeyValues[block.KeyValue_num - 1].key);
+                    insertKeyvalue(*block,KeyValue);
+                    index->KeyValue_num = block->KeyValue_num;
+                    if (strcmp(block->KeyValues[block->KeyValue_num - 1].key, index->end_key) != 0 || block->KeyValue_num == 1) {
+                        strcpy(index->end_key, block->KeyValues[block->KeyValue_num - 1].key);
                     }
     
-                    if (strcmp(block.KeyValues[0].key, index.beg_key) != 0 || block.KeyValue_num == 1) {
-                        strcpy(index.beg_key, block.KeyValues[0].key);
+                    if (strcmp(block->KeyValues[0].key, index->beg_key) != 0 || block->KeyValue_num == 1) {
+                        strcpy(index->beg_key, block->KeyValues[0].key);
                     }
     
-                    index_file.update(index, index_pos);
-                    block_file.update(block, block_pos);
+                    index_file.update(*index, index_pos);
+                    block_file.update(*block, block_pos);
                     break;
                 }
             } else {
-                index_pos = index.next_offset;
+                index_pos = index->next_offset;
             }
         }
     }
     void DeleteKeyValue(const std::string &key, const T &value) {
         Key_Value<T> KeyValue(key, value);
-        Index index;
-        Block<T> block;
+        auto index = std::make_unique<Index>();
+        auto block = std::make_unique<Block<T>>();
         int index_pos = 0;
         index_file.get_info(index_pos, 2);
         int prev_pos = -1, prev_KeyValue_num = -1, index_number;
         index_file.get_info(index_number, 1);
     
         for (int i = 0; i < index_number; ++i) {
-            index_file.read(index, index_pos);
+            index_file.read(*index, index_pos);
             //查询直到比自己大的key
-            if (strcmp(index.beg_key, key.c_str()) > 0) {
+            if (strcmp(index->beg_key, key.c_str()) > 0) {
                 return;
             }
             //如果在这个block内
-            if (strcmp(index.end_key, key.c_str()) >= 0) {
-                block_file.read(block, index.offset);
-                for (int j = 0; j < block.KeyValue_num; ++j) {
-                    if ((block.KeyValues[j].value == KeyValue.value) && (strcmp(KeyValue.key, block.KeyValues[j].key) == 0)) {
+            if (strcmp(index->end_key, key.c_str()) >= 0) {
+                block_file.read(*block, index->offset);
+                for (int j = 0; j < block->KeyValue_num; ++j) {
+                    if ((block->KeyValues[j].value == KeyValue.value) && (strcmp(KeyValue.key, block->KeyValues[j].key) == 0)) {
                         //删除，键值块前移
-                        for (int k = j; k < block.KeyValue_num - 1; ++k) {
-                            block.KeyValues[k] = block.KeyValues[k + 1];
+                        for (int k = j; k < block->KeyValue_num - 1; ++k) {
+                            block->KeyValues[k] = block->KeyValues[k + 1];
                         }
-                        block.KeyValue_num--;
+                        block->KeyValue_num--;
                         //如果删了唯一元素
-                        if (block.KeyValue_num == 0) {
+                        if (block->KeyValue_num == 0) {
                             if (prev_pos != -1) {
-                                Index prev_index;
-                                index_file.read(prev_index, prev_pos);
-                                prev_index.next_offset = index.next_offset;
-                                index_file.update(prev_index, prev_pos);
+                                auto prev_index = std::make_unique<Index>();
+                                index_file.read(*prev_index, prev_pos);
+                                prev_index->next_offset = index->next_offset;
+                                index_file.update(*prev_index, prev_pos);
                             } else {
-                                index_file.write_info(index.next_offset, 2);
+                                index_file.write_info(index->next_offset, 2);
                             }
                             index_file.write_info(index_number - 1, 1);
                         }
                         //与前一个块合并：减少读写次数
-                        else if (prev_pos != -1 && block.KeyValue_num <= BLOCK_SIZE / 2 && prev_KeyValue_num <= BLOCK_SIZE / 2) {
-                            Index prev_index;
-                            Block<T> prev_block;
+                        else if (prev_pos != -1 && block->KeyValue_num <= BLOCK_SIZE / 2 && prev_KeyValue_num <= BLOCK_SIZE / 2) {
+                            auto prev_index = std::make_unique<Index>();
+                            auto prev_block = std::make_unique<Block<T>>();
 
-                            index_file.read(prev_index, prev_pos);
-                            block_file.read(prev_block, prev_index.offset);
+                            index_file.read(*prev_index, prev_pos);
+                            block_file.read(*prev_block, prev_index->offset);
 
-                            prev_index.next_offset = index.next_offset;
+                            prev_index->next_offset = index->next_offset;
                             
                             //并块
                             Block<T> new_prev_block;
                             
-                            mergeBlocks(block, prev_block, new_prev_block);
-                            prev_index.KeyValue_num = new_prev_block.KeyValue_num;
+                            mergeBlocks(*block, *prev_block, new_prev_block);
+                            prev_index->KeyValue_num = new_prev_block.KeyValue_num;
 
-                            strcpy(prev_index.beg_key, new_prev_block.KeyValues[0].key);
-                            strcpy(prev_index.end_key, new_prev_block.KeyValues[prev_index.KeyValue_num - 1].key);
+                            strcpy(prev_index->beg_key, new_prev_block.KeyValues[0].key);
+                            strcpy(prev_index->end_key, new_prev_block.KeyValues[prev_index->KeyValue_num - 1].key);
 
-                            index_file.update(prev_index, prev_pos);
-                            block_file.update(new_prev_block, prev_index.offset);
+                            index_file.update(*prev_index, prev_pos);
+                            block_file.update(new_prev_block, prev_index->offset);
                             index_file.write_info(index_number - 1, 1);
                         }
                         else {
-                            if (strcmp(block.KeyValues[0].key, index.beg_key) != 0) {
-                                strcpy(index.beg_key, block.KeyValues[0].key);
+                            if (strcmp(block->KeyValues[0].key, index->beg_key) != 0) {
+                                strcpy(index->beg_key, block->KeyValues[0].key);
                             }
-                            if (strcmp(block.KeyValues[block.KeyValue_num - 1].key, index.end_key) != 0) {
-                                strcpy(index.end_key, block.KeyValues[block.KeyValue_num - 1].key);
+                            if (strcmp(block->KeyValues[block->KeyValue_num - 1].key, index->end_key) != 0) {
+                                strcpy(index->end_key, block->KeyValues[block->KeyValue_num - 1].key);
                             }
-                            index.KeyValue_num = block.KeyValue_num;
-                            index_file.update(index, index_pos);
-                            block_file.update(block, index.offset);
+                            index->KeyValue_num = block->KeyValue_num;
+                            index_file.update(*index, index_pos);
+                            block_file.update(*block, index->offset);
                         }
                         return;
                     }
@@ -303,8 +312,8 @@ public:
             }
             //查询下一个节点
             prev_pos = index_pos;
-            index_pos = index.next_offset;
-            prev_KeyValue_num = index.KeyValue_num;
+            index_pos = index->next_offset;
+            prev_KeyValue_num = index->KeyValue_num;
         }
     }
     std::set<T> FindKey(const std::string &key) {
@@ -352,9 +361,40 @@ public:
         return false;
     }
     T FindSingle(const string& key) {
-        auto result = FindKey(key);
-        auto it = result.begin();
-        return *it;
+        int start = 0, index_number = 0;
+        T result;
+        index_file.get_info(start, 2);
+        index_file.get_info(index_number, 1);
+        int index_pos = start;
+        Index index;
+        Block<T> block;
+        for (int i = 0; i < index_number; ++i) {
+            index_file.read(index, index_pos);
+
+            if (strcmp(index.beg_key, key.c_str()) > 0) {
+                break;
+            }
+
+            if (strcmp(index.end_key, key.c_str()) >= 0) {
+                block_file.read(block, index.offset);
+                int l = 0, r = block.KeyValue_num - 1;
+                while (l < r) {
+                    int mid = l + (r - l) / 2; 
+                    if (strcmp(block.KeyValues[mid].key, key.c_str()) < 0) {
+                        l = mid + 1; 
+                    } else {
+                        r = mid;
+                    }
+                }
+                if (strcmp(block.KeyValues[l].key, key.c_str()) != 0) {
+                    l++;
+                }
+                result = block.KeyValues[l].value;
+
+            }
+            index_pos = index.next_offset;
+        }
+        return result;
     }
     int get_KeyValue_num() {
         int tmp;
